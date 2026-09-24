a3e81cf840e447925fcfdb9bce7ce8493eba853a
Arkadeep De <your_email@wilp.bits-pilani.ac.in>
2026-09-25 01:42:06 +0530

    Update message to trigger CI build

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -1,7 +1,7 @@
 package com.bits;
 public class App {
     public static String getMessage() {
-        return "Hello DevOps! Build 1 - Jenkins CI Lab by 2022WB86582";
+        return "Hello DevOps! Build 2 - Code change triggered Jenkins CI automatically";
     }
     public static void main(String[] args) {
         System.out.println(getMessage());
